8ac8e54dd762e57720177aea08712b7f6875a9d8
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-04-08 17:29:44 -0700

    Added cart button to navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,12 @@ function Navigation() {
     return (
         <nav>
             <h1>Nav Bar</h1>
+
+            <div>
+                <button>Cart</button>
+                <span>1 item</span>
+            </div>
+
             <Link to="home">Home Page</Link>
             <Link to="shop">Shop Page</Link>
         </nav>
